f3fb3dc4eddc40388a32a64bee35137116448179
jihanurrahman33 <jihanurrahman33@gmail.com>
2026-01-18 12:05:37 +0600

    feat: enhance proxy function for authentication; integrate token validation and session handling

--- src/proxy.js
+++ src/proxy.js
@@ -1,10 +1,20 @@
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+import { authOptions } from "./lib/authOptions";
+import { getToken } from "next-auth/jwt";
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request) {
+export async function proxy(request) {
+  //get session
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const isAuthenticated = Boolean(token);
+
   //get cookie
   const authCookie = request.cookies.get("auth");
-  if (authCookie && authCookie.value === "true") {
+  if ((authCookie && authCookie.value === "true") || isAuthenticated) {
     return NextResponse.next();
   }
   return NextResponse.redirect(new URL("/login", request.url));
