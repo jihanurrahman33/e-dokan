18ec9d4ad4d1a5f09c6113d93cf535097abc6429
jihanurrahman33 <jihanurrahman33@gmail.com>
2026-01-29 12:29:44 +0600

    fix: update user authentication check in AddToCart component to use useSession hook

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,18 +1,21 @@
 "use client";
+import { getSession, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "react-toastify";
 
 const AddToCart = () => {
   const router = useRouter();
+  const isLoggedIn = useSession().data?.user ? true : false;
   const handleAddToCart = () => {
     //first check if user is logged in
-    const isLoggedIn = Boolean(localStorage.getItem("userToken"));
+
     if (!isLoggedIn) {
       router.push("/login");
       return;
     }
     // Implement add to cart functionality here
+
     toast.success("Product added to cart!");
   };
   return (
