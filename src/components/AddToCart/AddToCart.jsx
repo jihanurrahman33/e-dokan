d03f161d8f44871c342a486c71d27c4be9a6170a
jihanurrahman33 <jihanurrahman33@gmail.com>
2026-01-29 12:24:42 +0600

    feat: add user authentication check before adding products to cart

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,9 +1,17 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "react-toastify";
 
 const AddToCart = () => {
+  const router = useRouter();
   const handleAddToCart = () => {
+    //first check if user is logged in
+    const isLoggedIn = Boolean(localStorage.getItem("userToken"));
+    if (!isLoggedIn) {
+      router.push("/login");
+      return;
+    }
     // Implement add to cart functionality here
     toast.success("Product added to cart!");
   };
